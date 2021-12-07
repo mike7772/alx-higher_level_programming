10a810c19c25d7ee6ab6ce0402aa6823126dfe2c
mike7772 <michaelasnake644@gmail.com>
2021-12-07 13:47:10 -0800

    classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Square Class
-A Square Class
-"""
+"""Define a class Square."""
+
 
 class Square:
     """Represent a square."""
@@ -57,3 +56,14 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
